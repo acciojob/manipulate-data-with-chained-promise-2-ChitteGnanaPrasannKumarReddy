586b1d4d6f74b5cebfffb7ad9d594d19c7dda375
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2025-04-19 16:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 //your JS code here. If required.
+function manipulateArray() {
+  const initialArray = [1, 2, 3, 4];
+  const outputDiv = document.getElementById('output');
 
-function filterOddNumbers(array) {
-    return new Promise((resolve) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(initialArray);
+    }, 3000);
+  })
+    .then((arr) => {
+      return new Promise((resolve) => {
         setTimeout(() => {
-            const evenNumbers = array.filter(num => num % 2 === 0);
-            document.getElementById('output').textContent = evenNumbers.join(', '); 
-            resolve(evenNumbers);
+          const evenNumbers = arr.filter((num) => num % 2 === 0);
+          outputDiv.textContent = evenNumbers.join(',');
+          resolve(evenNumbers);
         }, 1000);
+      });
+    })
+    .then((evenArr) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const multipliedNumbers = evenArr.map((num) => num * 2);
+          outputDiv.textContent = multipliedNumbers.join(',');
+          resolve();
+        }, 2000);
+      });
     });
 }
+
+manipulateArray();
